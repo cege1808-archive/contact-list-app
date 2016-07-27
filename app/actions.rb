8337b77b26c384e727bc6b98d47f4ae0becff833
cege1808 <gunawan.clarissa@hotmail.com>
2016-07-27 18:05:49 +0000

    retrieve contacts from database and display

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+get '/api/contacts' do
+  content_type :json
+  @all_contacts = Contact.order(:first_name)
+  @all_contacts.to_json
+end
+
+get '/api/contact/new' do
+  @contact = Contact.new
+  erb :new
+end
+
+post '/api/contact/create' do 
+  @contact = Contact.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    phone: params[:phone],
+    birthday: params[:birthday]
+    )
+  # persisted?
+end
+
+get '/contact/:contact_id' do
+
+end
+
+
+
+
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -9,10 +9,9 @@ $(document).ready(function() {
 
 
   function setTable(){
-    var table = $("table");
-    if (table.length) {
-      table.append("<tr></tr>");
-      var row = table.find("tr");
+    var head = $("table thead");
+      head.append("<tr></tr>");
+      var row = head.find("tr");
       var headingName = "<th>Full Name </th>" ;
       var headingImage = "<th></th>";
       var headingEmail = "<th>Email</th>";
@@ -23,30 +22,44 @@ $(document).ready(function() {
       row.append(headingEmail);
       row.append(headingPhone);
       row.append(headingBirthday);
-    }
+
 
   }
 
   function insertContacts(){
+    setTable();
     $.ajax({
       url: '/api/contacts',
       method: 'GET',
       success: function(data){
-        $.each(data, function(index, contact)){
+        console.log(data);
+        $.each(data, function(index, contact){
+          console.log(contact);
 
-        }
-        var table = $("table");
-        var contentName = "<td>"+ data.name +"</td>" ;
-        var contentImage = "<td></td>";
-        var contentEmail = "<td>Email</td>";
-        var contentPhone = "<td>Phone</td>";
-        var contentBirthday = "<td>Birthday</td>";
-      };
+          var body = $("table tbody");
+          body.append("<tr></tr>");
+          var row = body.find("tr").last(); 
+          
+
+          var contentName = "<td>"+ contact.first_name +"</td>" ;
+          var contentImage = "<td><img class='user-image' src='http://cdn.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-user-19.png' /></td>";
+          var contentEmail = "<td>"+ contact.email +"</td>";
+          var contentPhone = "<td>"+ contact.phone +"</td>";
+          var contentBirthday = "<td>"+ contact.birthday +"</td>";
+
+          row.append(contentName);
+          row.append(contentImage);
+          row.append(contentEmail);
+          row.append(contentPhone);
+          row.append(contentBirthday);
+        });
+      }
     });
 
   }
 
-  setTable();
+  
+  insertContacts();
   // $.ajax({
   //   url: '/api/contact/create',
   //   method: 'POST',
